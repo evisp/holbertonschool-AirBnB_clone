f8bc10c1c44f133d18aff9761438690a1fc09dde
evisp <evisplaku@gmail.com>
2022-06-28 00:18:09 -0700

    Fix: comments style

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" FileStorage class """
+""" FileStorage class 
+"""
 import json
 from models.base_model import BaseModel
 import datetime
@@ -33,7 +34,8 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        """ all method that returns the dictionary __objects """
+        """ all method that returns the dictionary __objects 
+        """
         return FileStorage.__objects
 
     def new(self, obj):
@@ -43,15 +45,15 @@ class FileStorage:
         obj_id = obj.id
         obj_key = obj_name + "." + obj_id
         FileStorage.__objects[obj_key] = obj
-        """
-        if obj:
+        """ if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             FileStorage.__objects[key] = obj
         """
 
     def save(self):
         """ save method serializes __objects to the JSON file
-        (path: __file_path) """
+        (path: __file_path) 
+        """
         with open(FileStorage.__file_path, encoding='utf-8', mode='w') as file:
             new_d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(new_d, file)
@@ -60,7 +62,8 @@ class FileStorage:
         """ deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists;
         otherwise, do nothing. If the file doesnt exist,
-        no exception should be raised) """
+        no exception should be raised) 
+        """
         try:
             with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
                 for key, value in (json.load(f)).items():
