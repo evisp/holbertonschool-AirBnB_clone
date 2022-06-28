e10653b11cce40619edd328d4db5fbb93bf95fdd
evisp <evisplaku@gmail.com>
2022-06-28 00:26:06 -0700

    Fix: comments style

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" FileStorage class
+"""FileStorage class
 """
 import json
 from models.base_model import BaseModel
@@ -14,7 +14,7 @@ from models.review import Review
 
 
 class FileStorage:
-    """ serializes instances to a JSON file and deserializes JSON file
+    """serializes instances to a JSON file and deserializes JSON file
     to instances.
 
     ATTRIBUTES:
@@ -34,24 +34,24 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        """ all method that returns the dictionary __objects
+        """all method that returns the dictionary __objects
         """
         return FileStorage.__objects
 
     def new(self, obj):
-        """ new method sets in __objects the obj with
+        """new method sets in __objects the obj with
         key <obj class name>.id """
         obj_name = type(obj).__name__
         obj_id = obj.id
         obj_key = obj_name + "." + obj_id
         FileStorage.__objects[obj_key] = obj
-        """ if obj:
+        """if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             FileStorage.__objects[key] = obj
         """
 
     def save(self):
-        """ save method serializes __objects to the JSON file
+        """save method serializes __objects to the JSON file
         (path: __file_path)
         """
         with open(FileStorage.__file_path, encoding='utf-8', mode='w') as file:
@@ -59,7 +59,7 @@ class FileStorage:
             json.dump(new_d, file)
 
     def reload(self):
-        """ deserializes the JSON file to __objects
+        """deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists;
         otherwise, do nothing. If the file doesnt exist,
         no exception should be raised)
